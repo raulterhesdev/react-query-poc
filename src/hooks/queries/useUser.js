744cfdd3ac2a912fe89df5c7bc2053849ec50b8b
raulterhes <raul.terhes@toluna.com>
2021-02-18 14:47:18 +0200

    Modified to get the uid of the logged user

--- src/hooks/queries/useUser.js
+++ src/hooks/queries/useUser.js
@@ -1,6 +1,9 @@
 import { useQuery } from "react-query";
 import { getUser } from "../../utils/firebaseAPI";
+import { useAuth } from "../../context/auth-context";
 
-export const useUser = (uid) => {
+export const useUser = () => {
+	const { user } = useAuth();
+	const uid = user.user.uid;
 	return useQuery(["users", uid], () => getUser(uid));
 };
