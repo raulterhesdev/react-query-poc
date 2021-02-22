d01b139239c2da567716b5f70fae1f8d3649160a
raulterhes <raul.terhes@toluna.com>
2021-02-22 15:28:28 +0200

    added placeholder data

--- src/hooks/queries/useTask.js
+++ src/hooks/queries/useTask.js
@@ -1,6 +1,12 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import { getTask } from "../../utils/firebaseAPI";
 
 export const useTask = (taskId) => {
-	return useQuery(["tasks", taskId], () => getTask(taskId));
+	const queryClient = useQueryClient();
+
+	return useQuery(["tasks", taskId], () => getTask(taskId), {
+		placeholderData: () => {
+			return queryClient.getQueryData("tasks")?.find((p) => p.id === taskId);
+		},
+	});
 };
