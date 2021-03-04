9beff315e81a2aa4d84033c6a2355f1d83b6110e
raulterhes <raul.terhes@toluna.com>
2021-03-04 10:59:33 +0200

    added owner data/link + created/updated dates

--- src/pages/Project/Project.page.js
+++ src/pages/Project/Project.page.js
@@ -13,8 +13,22 @@ import { Option, Select } from "../../components/Select/Select";
 import Button from "../../components/Button/Button";
 import ProjectTasks from "./ProjectTasks";
 import QueryWrapper from "../../components/QueryWrapper/QueryWrapper";
+import Link from "../../components/Link/Link";
 
 import { categories } from "../../utils/constants";
+import { useUser, useLoggedUser } from "../../hooks/queries/userQueries";
+
+const Owner = ({ ownerId }) => {
+	const { data } = useUser(ownerId);
+	return (
+		<div className='w-full p-2'>
+			<p className='text-yellow-900'>Owner:</p>
+			<p>
+				<Link to={`/user/${data?.uid}`}>{data?.name || "..."}</Link>
+			</p>
+		</div>
+	);
+};
 
 const Project = () => {
 	const params = useParams();
@@ -24,6 +38,7 @@ const Project = () => {
 	const updateProjectMutation = useUpdateProject();
 	const [description, setDescription] = useState("");
 	const [category, setCategory] = useState("");
+	const loggedUser = useLoggedUser();
 
 	useEffect(() => {
 		setDescription(data?.description || "");
@@ -39,6 +54,8 @@ const Project = () => {
 		updateProjectMutation.mutate({ ...data, description, category });
 	};
 
+	const canUpdateDelete = loggedUser.data?.uid === data?.ownerUid;
+
 	return (
 		<Layout pageTitle={data?.name}>
 			<div className='px-6 py-4 flex flex-col justify-center items-center'>
@@ -50,6 +67,7 @@ const Project = () => {
 					{data ? (
 						<div className='flex'>
 							<div className='flex flex-col items-center mr-8'>
+								<Owner ownerId={data.ownerUid} />
 								<Textarea
 									value={description}
 									onChange={(e) => setDescription(e.target.value)}
@@ -66,28 +84,38 @@ const Project = () => {
 										<Option key={c} value={c} text={c} />
 									))}
 								</Select>
-								<div className='my-4'>
-									<QueryWrapper
-										isLoading={updateProjectMutation.isLoading}
-										error={updateProjectMutation.error}
-										errorText='There was an error updating the project data.'
-									>
-										<Button onClick={submitUpdate} text='Update Project' />
-									</QueryWrapper>
-								</div>
-								<div className='mb-4'>
-									<QueryWrapper
-										isLoading={deleteProjectMutation.isLoading}
-										error={deleteProjectMutation.error}
-										errorText='There was an error deleting the project.'
-									>
-										<Button
-											type='danger'
-											text='Delete Project'
-											onClick={submitDelete}
-										/>
-									</QueryWrapper>
-								</div>
+								<p className='w-full text-yellow-900 px-2 py-1 mt-2'>
+									Created at: {data.createdAt}
+								</p>
+								<p className='w-full text-yellow-900 px-2 py-1'>
+									Last Updated: {data.updatedAt}
+								</p>
+								{canUpdateDelete ? (
+									<>
+										<div className='my-4'>
+											<QueryWrapper
+												isLoading={updateProjectMutation.isLoading}
+												error={updateProjectMutation.error}
+												errorText='There was an error updating the project data.'
+											>
+												<Button onClick={submitUpdate} text='Update Project' />
+											</QueryWrapper>
+										</div>
+										<div className='mb-4'>
+											<QueryWrapper
+												isLoading={deleteProjectMutation.isLoading}
+												error={deleteProjectMutation.error}
+												errorText='There was an error deleting the project.'
+											>
+												<Button
+													type='danger'
+													text='Delete Project'
+													onClick={submitDelete}
+												/>
+											</QueryWrapper>
+										</div>
+									</>
+								) : null}
 							</div>
 							<ProjectTasks projectId={data.id} />
 						</div>
