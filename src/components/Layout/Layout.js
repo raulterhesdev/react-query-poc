6363d0c33c2ba548dfb1fa34b301f5ac81c8826a
raulterhes <raul.terhes@toluna.com>
2021-03-10 15:49:24 +0200

    added welcome message

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,15 @@
 import React from "react";
 import PageTitle from "../PageTitle/PageTitle";
+import { useLoggedUser } from "../../hooks/queries/userQueries";
 
 const Layout = ({ pageTitle, children }) => {
+	const { data: userData } = useLoggedUser();
 	return (
 		<div className=' w-full ml-40'>
 			<PageTitle title={pageTitle || "..."} />
+			<div className='absolute top-2 right-2 text-white text-xl '>
+				<p>Hello, {userData?.name}</p>
+			</div>
 			{children}
 		</div>
 	);
